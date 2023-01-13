5e0d0b752adbfde7cabad0ea0d8f55e392f8c6ad
dev-choee <rhksgh99@gmail.com>
2023-01-13 09:54:28 +0900

    gwanho JUnit Assert에서 AssertJ로 코드를 직관적으로 수정

--- calculator/src/test/java/CalculatorTest.java
+++ calculator/src/test/java/CalculatorTest.java
@@ -1,31 +1,31 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
     @Test
     @DisplayName("더하기 테스트")
     public void add(){
         Calculator cal = new Calculator();
-        assertEquals(12, cal.add(9,3));
+        assertThat(cal.add(9,3)).isEqualTo(12);
     }
     @Test
     @DisplayName("빼기 테스트")
     public void subtract(){
         Calculator cal = new Calculator();
-        assertEquals(6, cal.subtract(9,3));
+        assertThat(cal.subtract(9,3)).isEqualTo(6);
     }
     @Test
     @DisplayName("곱하기 테스트")
     public void multiply(){
         Calculator cal = new Calculator();
-        assertEquals(27, cal.multiply(9,3));
+        assertThat(cal.multiply(9,3)).isEqualTo(27);
     }
     @Test
     @DisplayName("나누기 테스트")
     public void divide() {
         Calculator cal = new Calculator();
-        assertEquals(3, cal.divide(9,3));
+        assertThat(cal.divide(9,3)).isEqualTo(3);
     }
 }
