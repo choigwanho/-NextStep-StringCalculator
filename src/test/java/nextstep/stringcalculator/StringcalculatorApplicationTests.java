6ba7be3c1eaca6d8562d7d362cbe89cdddbeea81
namgon <nglee.dev@gmail.com>
2023-01-15 14:06:02 +0900

    init

---
+++ src/test/java/nextstep/stringcalculator/StringcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package nextstep.stringcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
