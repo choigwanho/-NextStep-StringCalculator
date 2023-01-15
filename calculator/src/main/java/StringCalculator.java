4c8a44bd3066591dbcc92c41ccd1b0ec389a364c
dev-choee <rhksgh99@gmail.com>
2023-01-15 21:18:26 +0900

    gwanho StringCalculator 로직 구현 완료 및 테스트 코드 성공 확인

--- calculator/src/main/java/StringCalculator.java
+++ calculator/src/main/java/StringCalculator.java
@@ -1,13 +1,40 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     /* TASK
-    - 전달하는 문자를 구분자로 분리한 후 각 숫자의 합을 구해 반환
-    - 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
-    - 앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있음
-      - 커스텀 구분자는 문자열 앞부분의 "//"와 "\n" 사이에 위치한 문자를 커스텀 구분자로 사용
-    - 문자열 계산기에 음수를 전달하는 경우 RuntimeException으로 예외 처리
+        - 전달하는 문자를 구분자로 분리한 후 각 숫자의 합을 구해 반환
+        - 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
+        - 앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있음
+          - 커스텀 구분자는 문자열 앞부분의 "//"와 "\n" 사이에 위치한 문자를 커스텀 구분자로 사용
+        - 문자열 계산기에 음수를 전달하는 경우 RuntimeException으로 예외 처리
     */
-    int add(String text){
-        throw new RuntimeException("음수가 입력됐습니다.");
+    public int add(String text){
+
+        // 빈 문자열 또는 null 값을 입력할 경우 0을 반환
+        if (text == null || text.isEmpty()) return 0;
+
+        // "//"와 "\n" 문자 사이에 커스텀 구분자 지정
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        String[] values;
+
+        if (m.find()) {
+            String customDelimeter = m.group(1);
+            System.out.println(customDelimeter);
+            values = m.group(2).split(customDelimeter);
+        }else{
+            values = text.split(",|:");
+        }
+
+        int sum = 0;
+        for (String value: values){
+            // 음수를 전달하는 경우 RuntimeException 예외 처리
+            if (Integer.parseInt(value) < 0) throw new RuntimeException("음수가 입력됐습니다.");
+            sum += Integer.parseInt(value);
+        }
+
+        return sum;
     }
+
 }
--- calculator/src/test/java/StringCalculatorTest.java
+++ calculator/src/test/java/StringCalculatorTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,28 +17,34 @@ public class StringCalculatorTest {
     }
 
     @Test
+    @DisplayName("빈 문자열 또는 null 값 입력시 0 반환")
     public void add_null_또는_빈문자(){
         assertThat(strCal.add(null)).isEqualTo(0);
-        assertThat(strCal.add(" ")).isEqualTo(0);
+        assertThat(strCal.add("")).isEqualTo(0);
     }
     @Test
-    public void add_한문자(){
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환")
+    public void add_숫자하나(){
         assertThat(strCal.add("1")).isEqualTo(1);
     }
     @Test
-    public void add_쉼표_구분자(){
+    @DisplayName("쉼표 구분자를 사용해 숫자 두개 이상 입력할 경우 숫자의 합을 반환")
+    public void add_쉼표구분자(){
         assertThat(strCal.add("1,2")).isEqualTo(3);
         assertThat(strCal.add("1,2,3")).isEqualTo(6);
     }
     @Test
+    @DisplayName("쉼표 이외에 콜론 구분자를 사용해 숫자 두개 이상 입력할 경우 숫자의 합을 반환")
     public void add_쉼표_또는_콜론_구분자(){
         assertThat(strCal.add("1,2:3")).isEqualTo(6);
     }
     @Test
+    @DisplayName("커스텀 구분자를 사용해 숫자 두개 이상 입력할 경우 숫자의 합을 반환")
     public void add_커스텀_구분자(){
         assertThat(strCal.add("//;\n1;2;3")).isEqualTo(6);
     }
     @Test
+    @DisplayName("음수를 전달하는 경우 RuntimeException 예외 처리")
     public void add_음수_예외_처리(){
         assertThrows(RuntimeException.class, () -> {
             strCal.add("1,-1,3");
